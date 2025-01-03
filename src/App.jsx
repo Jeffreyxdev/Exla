e9f835685733e931d69ab138054824c8583cabe2
jeffrey <agabaenwerejeffrey@gmail.com>
2025-01-03 21:43:43 +0000

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
+import Events from './InnerPages/Events'
+import Guide from './Pages/Guide'
+import Login from './Pages/Login'
+import Signup from './Pages/signup'
+import Pricing from './Pages/Pricing'
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/discover-events" element={<Events/>} />
+        <Route path="/How-exla-works" element={<Guide/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Signup/>} />
+        <Route path="/" element={<Pricing />} />
+
         
 
       </Routes>
