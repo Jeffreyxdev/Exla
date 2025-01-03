ad770a58a67e1a73eba1b2b5e7940b57c335b308
jeffrey <agabaenwerejeffrey@gmail.com>
2025-01-03 21:36:30 +0000

    works

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Router } from 'express'
+
 
 createRoot(document.getElementById('root')).render(
 <Router>
