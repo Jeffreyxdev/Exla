bb613b3e38fe4a2f8dcacb56eb5987ea9322cd30
jeffrey <agabaenwerejeffrey@gmail.com>
2025-01-08 12:26:08 +0000

    v

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
     signOut(auth).then(() => {
       localStorage.clear();
       setIsAuth(false);
-    window.location.pathname = "/login"
+    windo.location.pathname = "/login"
     })
   }
 
